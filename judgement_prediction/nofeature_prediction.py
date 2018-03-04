5d3ccc124a33d733ee186be482aae960630bcda2
guoxiaobo96 <guoxiaobo96@gmail.com>
2018-03-04 01:00:40 -0500

    enter

--- judgement_prediction/nofeature_prediction.py
+++ judgement_prediction/nofeature_prediction.py
@@ -28,7 +28,7 @@ def xls2txt(foldname='D:/案件数据/故意杀人案'):
         content = str()
         for _ in range(1, sheet.nrows):
             case_number +=1
-            content = sheet.cell(_, 13).value+" "+sheet.cell(_, 7).value+sheet.cell(_, 8).value+" "
+            content = sheet.cell(_, 13).value+" "+sheet.cell(_, 7).value+sheet.cell(_, 8).value+" "+sheet.cell(_, 11).value[1:4]
             position = content.find("本院认为")
             if position>0:
                 content = content[position:]
@@ -76,51 +76,6 @@ def getLabel(content):
 #            return "1\n"
     else:
         return "1\n", 1
-
-def load_data(fold_name):
-    """get texts and labels"""
-    train_texts = open(fold_name+'/train_content.txt', encoding='utf-8').read().split('\n')
-    train_labels = open(fold_name+'/train_label.txt', encoding='utf-8').read().split('\n')
-    test_texts = open(fold_name+'/test_content.txt', encoding='utf-8').read().split('\n')
-    test_labels = open(fold_name+'/test_label.txt', encoding='utf-8').read().split('\n')
-    print("load data……")
-    return train_texts, train_labels, test_texts, test_labels
-
-def doc2vec(train_texts, train_labels, test_texts, test_labels):
-    """convert the doc to vec"""
-    print("doc to vec")
-    from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
-    count_v0= CountVectorizer() 
-    counts_all = count_v0.fit_transform(train_texts+test_texts)
-    counts_train= CountVectorizer(vocabulary=count_v0.vocabulary_).fit_transform(train_texts)   
-    print ("the shape of train is "+repr(counts_train.shape))  
-    counts_test = CountVectorizer(vocabulary=count_v0.vocabulary_).fit_transform(test_texts)  
-    print ("the shape of test is "+repr(counts_test.shape))  
-  
-    tfidftransformer = TfidfTransformer()
-    train_data = tfidftransformer.fit_transform(counts_train)
-    test_data = tfidftransformer.fit_transform(counts_test) 
-
-    return train_data, train_labels, test_data, test_labels
-
-def svm(train_data, train_labels, test_data, test_labels):
-    """use line svm with line kernal and ovr mode"""
-    print("sym……")
-    from sklearn.svm import LinearSVC
-    import numpy
-    svclf = LinearSVC() 
-    svm_feature_list=svclf.fit(train_data,train_labels).coef_
-    numpy.savetxt("svm.txt", svm_feature_list)
-    with open(file="svm.txt", mode="a",encoding='utf-8') as target_file:
-        for i, svm_feature in enumerate(svm_feature_list):
-            numpy.savetxt("svm"+str(i)+".txt", svm_feature)
-    preds = svclf.predict(test_data)
-    num = 0
-    preds = preds.tolist()
-    for i,pred in enumerate(preds):
-        if int(pred) == int(test_labels[i]):
-            num += 1
-    print ('precision_score: '+str(float(num) / len(preds)))
 
 def main():
     xls2txt()
