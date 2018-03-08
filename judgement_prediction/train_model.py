e844a4c69943b91796a0661fb78d12a972f927eb
guoxiaobo96 <guoxiaobo96@gmail.com>
2018-03-08 10:12:12 -0500

    cnn

--- judgement_prediction/train_model.py
+++ judgement_prediction/train_model.py
@@ -9,6 +9,7 @@ def get_data(filename='D:/judgement_prediction/judgement_prediction/temp/data.tx
     from sklearn.model_selection import train_test_split
     from keras.preprocessing.text import Tokenizer
     from keras.preprocessing.sequence import pad_sequences
+    from keras.utils import to_categorical
     import pandas as pd
     import numpy as np
     print("getting data......")
@@ -16,7 +17,7 @@ def get_data(filename='D:/judgement_prediction/judgement_prediction/temp/data.tx
     data = pd.read_csv(filename, encoding='utf-8', sep=', ', header=None, names=columns, engine='python')
     data.reindex(np.random.permutation(data.index))
     content = data['content']
-    label = data['label']
+    label = to_categorical(np.array(data['label']))
     MAX_LEN = 200
     train_data, test_data, train_label, test_label = train_test_split(content, label,
                                                                       test_size=0.1, random_state=42)
