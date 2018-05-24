7728526e385f7114e6da62a651d0820604128792
晓波 郭 <guoxaiobo96@gmail.com>
2018-05-24 19:29:12 +0800

    test

--- hierachy/run_model.py
+++ hierachy/run_model.py
@@ -94,7 +94,7 @@ def train(x_train,y_train,x_val,y_val):
     total_batch = 0  # 总批次
     best_acc_val = 0.0  # 最佳验证集准确率
     last_improved = 0  # 记录上一次提升批次
-    require_improvement = 1000  # 如果超过1000轮未提升，提前结束训练
+    require_improvement = 3000  # 如果超过1000轮未提升，提前结束训练
 
     flag = False
     for _ in range(config.num_epochs):
