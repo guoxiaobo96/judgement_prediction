4525710d6c3ba774ac4ee9791ec0b462a54d2dfe
晓波 郭 <guoxaiobo96@gmail.com>
2018-05-24 18:30:46 +0800

    test

--- hierachy/cnnModelTest.py
+++ hierachy/cnnModelTest.py
@@ -7,7 +7,7 @@ class TCNNConfig(object):
 
     embedding_size = 128  # 词向量维度
     seq_length = 500  # 序列长度
-    num_classes = 41  # 类别数
+    num_classes = 5  # 类别数
     num_filters = 256  # 卷积核数目
     filter_size = [2,3,4,5]  # 卷积核尺寸
     vocab_size = 5000  # 词汇表大小
@@ -317,6 +317,72 @@ class TestCnnConv2(object):
             # 损失函数，交叉熵
             cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=self.logits, labels=self.y)
             self.loss = tf.reduce_mean(cross_entropy)+self.l2_loss*self.l2_lambda
+            # 优化器
+            self.optimizer = tf.train.AdamOptimizer(learning_rate=self.config.learning_rate).minimize(self.loss)
+
+        with tf.name_scope("accuracy"):
+            # 准确率
+            correct_pred = tf.equal(tf.argmax(self.y, 1), self.y_pred)
+            self.precision = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+class HierachyCnn(object):
+    """文本分类，CNN模型"""
+
+    def __init__(self, config):
+        self.config = config
+        # 三个待输入的数据
+        self.x = tf.placeholder(tf.int32, [None, self.config.seq_length], name='x')
+        self.y = tf.placeholder(tf.float32, [None, self.config.num_classes], name='y')
+        self.keep_prob = tf.placeholder(tf.float32, name='keep_prob')
+        self.l2_loss=0
+        self.l2_lambda=0.1
+
+        self.char_level_cnn()
+
+    def char_level_cnn(self):
+        """CNN模型"""
+        # 词向量映射
+        with tf.device('/cpu:0'):
+            embedding = tf.get_variable('embedding', [self.config.vocab_size, self.config.embedding_size])
+            embedding_inputs = tf.nn.embedding_lookup(embedding, self.x)
+
+        with tf.name_scope("cnn"):
+            # CNN layer
+            conv_1 = tf.layers.conv1d(embedding_inputs, 64, 3, name='conv1',trainable=True,padding='same')
+            conv_1=tf.nn.relu(conv_1)
+            # global max pooling layer
+            max_pool_1=tf.layers.max_pooling1d(conv_1,2,strides=1,padding='same')
+            conv_2=tf.layers.conv1d(max_pool_1,64, 1, name='conv2')
+            conv_3=tf.layers.conv1d(conv_2,32, 3, name='conv3')
+            max_pool_3=tf.layers.max_pooling1d(conv_3,1,strides=1,padding='same')
+            gmp=tf.reduce_max(max_pool_3, reduction_indices=[1], name='gmp')
+            
+
+        with tf.name_scope("score"):
+            # 全连接层，后面接dropout以及relu激活
+            fc = tf.layers.dense(gmp, self.config.hidden_dim, name='fc1')
+            fc = tf.contrib.layers.dropout(fc, self.keep_prob)
+            W=tf.get_variable("W",shape=[self.config.hidden_dim,41],initializer=tf.contrib.layers.xavier_initializer())
+            b = tf.Variable(tf.constant(0.1, shape=[41]), name="b")
+            self.l2_loss += tf.nn.l2_loss(W)
+            self.l2_loss += tf.nn.l2_loss(b)
+            self.logits=tf.nn.xw_plus_b(fc,W,b)
+            temp=[]
+            temp.append(tf.slice(self.logits,[0,0],[-1,1]))
+            temp.append(tf.slice(self.logits,[0,1],[-1,9]))
+            temp.append(tf.slice(self.logits,[0,10],[-1,16]))
+            temp.append(tf.slice(self.logits,[0,30],[-1,1]))
+            temp.append(tf.slice(self.logits,[0,40],[-1,1]))
+            for i in range(4):
+                temp[i]=tf.reduce_mean(temp[i],1,keepdims=True)
+            self.logits=tf.concat([temp[0],temp[1],temp[2],temp[3],temp[4]],axis=1)
+            print(self.logits.get_shape())
+            self.y_pred = tf.argmax(tf.nn.softmax(self.logits), 1)  # 预测类别
+
+        with tf.name_scope("optimize"):
+            # 损失函数，交叉熵
+            cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=self.logits, labels=self.y)
+            self.loss=tf.reduce_mean(cross_entropy)+self.l2_loss*self.l2_lambda
             # 优化器
             self.optimizer = tf.train.AdamOptimizer(learning_rate=self.config.learning_rate).minimize(self.loss)
 
