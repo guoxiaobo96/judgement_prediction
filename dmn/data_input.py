24790b89a3ef3ad1be23ec8e3016c95d267850ae
晓波 郭 <guoxiaobo96@gmail.com>
2018-06-07 20:44:19 +0800

    dmn数据测试用

--- dmn/data_input.py
+++ dmn/data_input.py
@@ -1,332 +1,68 @@
-import os as os
+import os
+import random
 import numpy as np
+import keras
 
 # can be sentence or word
 INPUT_MASK_MODE = "sentence"
 
 
-def init_data(fname):
-
-    print("==> Loading test from %s" % fname)
-    tasks = []
-    task = None
-    for _, line in enumerate(open(fname)):
-        index = int(line[0:line.find(' ')])
-        if index == 1:
-            task = {"C": "", "Q": "", "A": "", "S": ""}
-            counter = 0
-            id_map = {}
-
-        line = line.strip()
-        line = line.replace('.', ' . ')
-        line = line[line.find(' ')+1:]
-        # if not a question
-        if line.find('?') == -1:
-            task["C"] += line
-            id_map[index] = counter
-            counter += 1
-
-        else:
-            idx = line.find('?')
-            tmp = line[idx+1:].split('\t')
-            task["Q"] = line[:idx]
-            task["A"] = tmp[1].strip()
-            task["S"] = []
-            for num in tmp[2].split():
-                task["S"].append(id_map[int(num.strip())])
-            tasks.append(task.copy())
-
-    return tasks
+def get_data_raw(file_name):
+    data = []
+    with open(file=file_name, mode='r', encoding='utf8') as file:
+        for line in file:
+            content, label = line.strip().split(',')
+            if content:
+                data.append([content.split('。'), label])
+    return data
 
 
-def get_data_raw(index, test_id):
-    data_map = {
-        "1": "qa1_single-supporting-fact",
-        "2": "qa2_two-supporting-facts",
-        "3": "qa3_three-supporting-facts",
-        "4": "qa4_two-arg-relations",
-        "5": "qa5_three-arg-relations",
-        "6": "qa6_yes-no-questions",
-        "7": "qa7_counting",
-        "8": "qa8_lists-sets",
-        "9": "qa9_simple-negation",
-        "10": "qa10_indefinite-knowledge",
-        "11": "qa11_basic-coreference",
-        "12": "qa12_conjunction",
-        "13": "qa13_compound-coreference",
-        "14": "qa14_time-reasoning",
-        "15": "qa15_basic-deduction",
-        "16": "qa16_basic-induction",
-        "17": "qa17_positional-reasoning",
-        "18": "qa18_size-reasoning",
-        "19": "qa19_path-finding",
-        "20": "qa20_agents-motivations",
-        "MCTest": "MCTest",
-        "19changed": "19changed",
-        "joint": "all_shuffled",
-        "sh1": "../shuffled/qa1_single-supporting-fact",
-        "sh2": "../shuffled/qa2_two-supporting-facts",
-        "sh3": "../shuffled/qa3_three-supporting-facts",
-        "sh4": "../shuffled/qa4_two-arg-relations",
-        "sh5": "../shuffled/qa5_three-arg-relations",
-        "sh6": "../shuffled/qa6_yes-no-questions",
-        "sh7": "../shuffled/qa7_counting",
-        "sh8": "../shuffled/qa8_lists-sets",
-        "sh9": "../shuffled/qa9_simple-negation",
-        "sh10": "../shuffled/qa10_indefinite-knowledge",
-        "sh11": "../shuffled/qa11_basic-coreference",
-        "sh12": "../shuffled/qa12_conjunction",
-        "sh13": "../shuffled/qa13_compound-coreference",
-        "sh14": "../shuffled/qa14_time-reasoning",
-        "sh15": "../shuffled/qa15_basic-deduction",
-        "sh16": "../shuffled/qa16_basic-induction",
-        "sh17": "../shuffled/qa17_positional-reasoning",
-        "sh18": "../shuffled/qa18_size-reasoning",
-        "sh19": "../shuffled/qa19_path-finding",
-        "sh20": "../shuffled/qa20_agents-motivations",
-    }
-    if (test_id == ""):
-        test_id = index
-    data_name = data_map[index]
-    data_test_name = data_map[test_id]
-    data_train_raw = init_data(os.path.join(os.path.dirname(
-        os.path.realpath(__file__)), 'data/en-10k/%s_train.txt' % data_name))
-    data_test_raw = init_data(os.path.join(os.path.dirname(
-        os.path.realpath(__file__)), 'data/en-10k/%s_test.txt' % data_test_name))
-    return data_train_raw, data_test_raw
+def read_vocab(vocab_dir):
+    with open(vocab_dir, mode='r', encoding='utf8') as vocab_file:
+        words = [_.strip() for _ in vocab_file.readlines()]
+        word_to_id = dict(zip(words, range(len(words))))
+    return words, word_to_id
 
 
-def load_glove(dim):
-    word2vec = {}
+def process_data(raw_data, word_to_id, train_rate=0.6, test_rate=0.2, split=True):
+    random.shuffle(raw_data)
+    content_id, label_id, document_pad = [], [], []
+    document_len, sentence_len = 0, 0
+    for _, content in enumerate(raw_data):
+        document_id = []
+        label_id = content[1]
+        if len(content[0]) > document_len:
+            document_len = len(content[0])
+        for _, sentence in enumerate(content[0]):
+            if len(content[0]) > sentence_len:
+                sentence = len(content[0])
+            sentence_id = []
+            sentence_id.append([word_to_id[x]
+                                for x in sentence if x in word_to_id])
+            document_id.append(sentence_id)
+        content_id.append(document_id)
 
-    print("==> loading glove")
-    with open(("./data/glove/glove.6B/glove.6B." + str(dim) + "d.txt")) as f:
-        for line in f:
-            l = line.split()
-            word2vec[l[0]] = map(float, l[1:])
-
-    print("==> glove is loaded")
-
-    return word2vec
-
-
-def create_vector(word, word2vec, word_vector_size, silent=True):
-    # if the word is missing from Glove, create some fake vector and store in glove!
-    vector = np.random.uniform(0.0, 1.0, (word_vector_size,))
-    word2vec[word] = vector
-    if (not silent):
-        print("utils.py::create_vector => %s is missing" % word)
-    return vector
-
-
-def process_word(word, word2vec, vocab, ivocab, word_vector_size, to_return="word2vec", silent=True):
-    if not word in word2vec:
-        create_vector(word, word2vec, word_vector_size, silent)
-    if not word in vocab:
-        next_index = len(vocab)
-        vocab[word] = next_index
-        ivocab[next_index] = word
-
-    if to_return == "word2vec":
-        return word2vec[word]
-    elif to_return == "index":
-        return vocab[word]
-    elif to_return == "onehot":
-        raise Exception("to_return = 'onehot' is not implemented yet")
+    for _, document in enumerate(content_id):
+        document_pad.append(
+            keras.preprocessing.sequence.pad_sequences(document, sentence_len))
+    x_data = keras.preprocessing.sequence.pad_sequences(
+        document_pad, document_len)
+    y_data = keras.utils.to_categorical(label_id)
+    data_size = len(x_data)
+    x_train = x_data[:int(train_rate*data_size)]
+    y_train = y_data[:int(train_rate*data_size)]
+    x_val = x_data[int(train_rate*data_size)+1:int((1-test_rate)*data_size)]
+    y_val = y_data[int(train_rate*data_size)+1:int((1-test_rate)*data_size)]
+    x_test = x_data[int((1-test_rate)*data_size)+1:]
+    y_test = y_data[int((1-test_rate)*data_size)+1:]
+    if not split:
+        return x_data, y_data
+    return x_train, y_train, x_val, y_val, x_test, y_test
 
 
-def process_input(data_raw, floatX, word2vec, vocab, ivocab, embed_size, split_sentences=False):
-    questions = []
-    inputs = []
-    answers = []
-    input_masks = []
-    for x in data_raw:
-        if split_sentences:
-            inp = x["C"].lower().split(' . ')
-            inp = [w for w in inp if len(w) > 0]
-            inp = [i.split() for i in inp]
-        else:
-            inp = x["C"].lower().split(' ')
-            inp = [w for w in inp if len(w) > 0]
-
-        q = x["Q"].lower().split(' ')
-        q = [w for w in q if len(w) > 0]
-
-        if split_sentences:
-            inp_vector = [[process_word(word=w,
-                                        word2vec=word2vec,
-                                        vocab=vocab,
-                                        ivocab=ivocab,
-                                        word_vector_size=embed_size,
-                                        to_return="index") for w in s] for s in inp]
-        else:
-            inp_vector = [process_word(word=w,
-                                       word2vec=word2vec,
-                                       vocab=vocab,
-                                       ivocab=ivocab,
-                                       word_vector_size=embed_size,
-                                       to_return="index") for w in inp]
-
-        q_vector = [process_word(word=w,
-                                 word2vec=word2vec,
-                                 vocab=vocab,
-                                 ivocab=ivocab,
-                                 word_vector_size=embed_size,
-                                 to_return="index") for w in q]
-
-        if split_sentences:
-            inputs.append(inp_vector)
-        else:
-            inputs.append(np.vstack(inp_vector).astype(floatX))
-        questions.append(np.vstack(q_vector).astype(floatX))
-        answers.append(process_word(word=x["A"],
-                                    word2vec=word2vec,
-                                    vocab=vocab,
-                                    ivocab=ivocab,
-                                    word_vector_size=embed_size,
-                                    to_return="index"))
-        # NOTE: here we assume the answer is one word!
-
-        if not split_sentences:
-            if INPUT_MASK_MODE == 'word':
-                input_masks.append(
-                    np.array([index for index, w in enumerate(inp)], dtype=np.int32))
-            elif INPUT_MASK_MODE == 'sentence':
-                input_masks.append(
-                    np.array([index for index, w in enumerate(inp) if w == '.'], dtype=np.int32))
-            else:
-                raise Exception("invalid input_mask_mode")
-
-    return inputs, questions, answers, input_masks
+def main():
+    get_data_raw('D:/criminal_data/criminal/data.txt')
 
 
-def get_lens(inputs):
-    lens = np.zeros((len(inputs)), dtype=int)
-    for i, t in enumerate(inputs):
-        lens[i] = t.shape[0]
-    return lens
-
-
-def get_sentence_lens(inputs):
-    lens = np.zeros((len(inputs)), dtype=int)
-    sen_lens = []
-    max_sen_lens = []
-    for i, t in enumerate(inputs):
-        sentence_lens = np.zeros((len(t)), dtype=int)
-        for j, s in enumerate(t):
-            sentence_lens[j] = len(s)
-        lens[i] = len(t)
-        sen_lens.append(sentence_lens)
-        max_sen_lens.append(np.max(sentence_lens))
-    return lens, sen_lens, max(max_sen_lens)
-
-
-def pad_inputs(inputs, lens, max_len, mode="", sen_lens=None, max_sen_len=None):
-    if mode == "mask":
-        padded = [np.pad(inp, (0, max_len - lens[i]), 'constant',
-                         constant_values=0) for i, inp in enumerate(inputs)]
-        return np.vstack(padded)
-
-    elif mode == "split_sentences":
-        padded = np.zeros((len(inputs), max_len, max_sen_len))
-        for i, inp in enumerate(inputs):
-            padded_sentences = [np.pad(s, (0, max_sen_len - sen_lens[i][j]),
-                                       'constant', constant_values=0) for j, s in enumerate(inp)]
-            # trim array according to max allowed inputs
-            if len(padded_sentences) > max_len:
-                padded_sentences = padded_sentences[(
-                    len(padded_sentences)-max_len):]
-                lens[i] = max_len
-            padded_sentences = np.vstack(padded_sentences)
-            padded_sentences = np.pad(padded_sentences, ((
-                0, max_len - lens[i]), (0, 0)), 'constant', constant_values=0)
-            padded[i] = padded_sentences
-        return padded
-
-    padded = [np.pad(np.squeeze(inp, axis=1), (0, max_len - lens[i]),
-                     'constant', constant_values=0) for i, inp in enumerate(inputs)]
-    return np.vstack(padded)
-
-
-def create_embedding(word2vec, ivocab, embed_size):
-    embedding = np.zeros((len(ivocab), embed_size))
-    for i in range(len(ivocab)):
-        word = ivocab[i]
-        embedding[i] = word2vec[word]
-    return embedding
-
-
-def load_data(config, split_sentences=False):
-    vocab = {}
-    ivocab = {}
-
-    data_train_raw, data_test_raw = get_data_raw(
-        config.data_id, config.data_test_id)
-
-    if config.word2vec_init:
-        assert config.embed_size == 100
-        word2vec = load_glove(config.embed_size)
-    else:
-        word2vec = {}
-
-    # set word at index zero to be end of sentence token so padding with zeros is consistent
-    process_word(word="<eos>",
-                 word2vec=word2vec,
-                 vocab=vocab,
-                 ivocab=ivocab,
-                 word_vector_size=config.embed_size,
-                 to_return="index")
-
-    print('==> get train inputs')
-    train_data = process_input(data_train_raw, config.floatX,
-                               word2vec, vocab, ivocab, config.embed_size, split_sentences)
-    print('==> get test inputs')
-    test_data = process_input(data_test_raw, config.floatX,
-                              word2vec, vocab, ivocab, config.embed_size, split_sentences)
-
-    if config.word2vec_init:
-        assert config.embed_size == 100
-        word_embedding = create_embedding(word2vec, ivocab, config.embed_size)
-    else:
-        word_embedding = np.random.uniform(-config.embedding_init,
-                                           config.embedding_init, (len(ivocab), config.embed_size))
-
-    inputs, questions, answers, input_masks = train_data if config.train_mode else test_data
-
-    if split_sentences:
-        input_lens, sen_lens, max_sen_len = get_sentence_lens(inputs)
-        max_mask_len = max_sen_len
-    else:
-        input_lens = get_lens(inputs)
-        mask_lens = get_lens(input_masks)
-        max_mask_len = np.max(mask_lens)
-
-    q_lens = get_lens(questions)
-
-    max_q_len = np.max(q_lens)
-    max_input_len = min(np.max(input_lens), config.max_allowed_inputs)
-
-    # pad out arrays to max
-    if split_sentences:
-        inputs = pad_inputs(inputs, input_lens, max_input_len,
-                            "split_sentences", sen_lens, max_sen_len)
-        input_masks = np.zeros(len(inputs))
-    else:
-        inputs = pad_inputs(inputs, input_lens, max_input_len)
-        input_masks = pad_inputs(input_masks, mask_lens, max_mask_len, "mask")
-
-    questions = pad_inputs(questions, q_lens, max_q_len)
-
-    answers = np.stack(answers)
-
-    if config.train_mode:
-        train = questions[:config.num_train], inputs[:config.num_train], q_lens[:config.num_train], input_lens[:
-                                                                                                               config.num_train], input_masks[:config.num_train], answers[:config.num_train]
-
-        valid = questions[config.num_train:], inputs[config.num_train:], q_lens[config.num_train:
-                                                                                ], input_lens[config.num_train:], input_masks[config.num_train:], answers[config.num_train:]
-        return train, valid, word_embedding, max_q_len, max_input_len, max_mask_len, len(vocab)
-
-    else:
-        test = questions, inputs, q_lens, input_lens, input_masks, answers
-        return test, word_embedding, max_q_len, max_input_len, max_mask_len, len(vocab)
+if __name__ == '__main__':
+    main()
